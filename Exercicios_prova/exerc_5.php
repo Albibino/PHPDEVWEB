227e7307bfda2bd495165cd8d1cc224a62ce7936
william wollert <williamwollert@gmail.com>
2024-10-01 10:31:36 -0300

    v1.9

---
+++ Exercicios_prova/exerc_5.php
@@ -0,0 +1,31 @@
+<?php
+
+    function CalculaTriangulo($base, $altura){
+        $resultado = ($base * $altura) / 2;
+        return $resultado;
+    }
+
+$base = 10;
+$altura = 5;
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 5 - William Wollert</title>
+    <style>
+        .resultado {
+            color: black;
+            font-size: 24px;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <h1 class="resultado"><?php echo "A área do triângulo retângulo é: " . CalculaTriangulo($base, $altura); ?></h1>
+</body>
+</html>
